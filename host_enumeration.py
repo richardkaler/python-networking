88fd9036c1cd8b2033b2ea6e3a4f50283a10c57f
richardkaler <31756802+richardkaler@users.noreply.github.com>
2023-10-21 01:09:16 -0400

    Create host_enumeration.py
    This is the first super handy Python script I have posted here. The script runs a variety of commands against a target host, then prints the output to a log file. I also like this script because it uses a prompt to ask the user for input and the password is kept hidden from stdout by way of the maskpass import. Cool stuff!

---
+++ host_enumeration.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+#NOTE: The shebang is only necessary if you are running this on Linux and want to run the script without navigating to the directory it resides in first
+
+import logging
+import paramiko
+from datetime import datetime
+import maskpass
+
+ipinput = input("Enter the IP address for the host: ")
+
+userinput = input("Enter the user name for ssh connection: ")
+
+inputpasswd = maskpass.askpass(mask="")
+print(inputpasswd)
+
+print("Processing script with credential provided")
+
+host = ipinput
+username = userinput
+password = inputpasswd
+commands = ("uptime -p", "python --version", "uname -a", "netstat -lt | head -45", "ip addr show", "hostname", "groups", "sudo -l")
+command_timeout = 15
+logfile = "/home/richard/tmp/ssh-target-enum.log"
+current_datetime = datetime.now()
+
+# Configure the logging module
+logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s - %(message)s')
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Connect to the SSH server
+client.connect(host, username=username, password=password)
+
+# Open the log file for writing
+with open(logfile, 'w') as log_file:
+    print("Piping results to " + logfile)
+    print("=============================================================================")
+    for command in commands:
+        stdin, stdout, stderr = client.exec_command(command, timeout=command_timeout)
+        log_file.write(f"Command: {command}\n")
+        try:
+            output = stdout.read().decode()
+            log_file.write(output)
+            log_file.write("\n")
+            print(output)
+        except paramiko.ssh_exception.SSHException as e:
+            log_file.write(f"Error executing command: {e}\n")
+        except Exception as e:
+            log_file.write(f"An unexpected error occurred: {e}\n")
+
+# Append the current date and time at the bottom of the log file
+log_file = open(logfile, 'a')  # Open the file in append mode
+log_file.write("=============================================================================\n")
+log_file.write("Finished generating log at: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+log_file.close()
+# Close the SSH connection
+client.close()
+
+
